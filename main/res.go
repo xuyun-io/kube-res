84ff3243c806277f6982aa155cad6c1e83027667
maxwell92 <710028463@qq.com>
2019-09-28 11:03:26 +0800

    add resources

--- main/res.go
+++ main/res.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 
-	"github.com/xuyun-io/kube-res/main/resources"
+	"github.com/xuyun-io/kube-res/main/resources/extensionsv1beta1"
+	"github.com/xuyun-io/kube-res/main/resources/corev1"
 
 	"github.com/mlycore/log"
 )
@@ -16,13 +17,13 @@ func main() {
 	c := KubernetesClientset()
 	log.Infof("this is a log")
 
-	if list, err := resources.Pod(c.Clientset, testns); err == nil {
+	if list, err := corev1.Pod(c.Clientset, testns); err == nil {
 		for _, p := range list.Items {
 			fmt.Printf("pod/%s/%s\n", p.Namespace, p.Name)
 		}
 	}
 
-	if list, err := resources.Deployments(c.Clientset, testns); err == nil {
+	if list, err := extensionsv1beta1.Deployments(c.Clientset, testns); err == nil {
 		for _, dp := range list.Items {
 			fmt.Printf("deployments/%s/%s\n", dp.Namespace, dp.Name)
 		}
---
+++ main/resources/corev1/corev1.go
@@ -0,0 +1,51 @@
+package resources
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func Pods(c *kubernetes.Clientset, namespace string) (*corev1.PodList, error) {
+	return c.CoreV1().Pods(namespace).List(metav1.ListOptions{})
+}
+
+func ConfigMaps(c *kubernetes.Clientset, namespace string) (*corev1.ConfigMapList, error) {
+	return c.CoreV1().ConfigMaps(namespace).List(metav1.ListOptions{})
+}
+
+func Endpoints(c *kubernetes.Clientset, namespace string) (*corev1.EndpointsList, error) {
+	return c.CoreV1().Endpoints(namespace).List(metav1.ListOptions{})
+}
+
+func LimitRanges(c *kubernetes.Clientset, namespace string) (*corev1.LimitRangeList, error) {
+	return c.CoreV1().LimitRanges(namespace).List(metav1.ListOptions{})
+}
+
+func PersistentVolumeClaims(c *kubernetes.Clientset, namespace string) (*corev1.PersistentVolumeClaimList, error) {
+	return c.CoreV1().PersistentVolumeClaims(namespace).List(metav1.ListOptions{})
+}
+
+func PersistentVolumes(c *kubernetes.Clientset, namespace string) (*corev1.PersistentVolumeList, error) {
+	return c.CoreV1().PersistentVolumes().List(metav1.ListOptions{})
+}
+
+func ReplicationControllers(c *kubernetes.Clientset, namespace string) (*corev1.ReplicationControllerList, error) {
+	return c.CoreV1().ReplicationControllers(namespace).List(metav1.ListOptions{})
+}
+
+func ResourceQuotas(c *kubernetes.Clientset, namespace string) (*corev1.ResourceQuotaList, error) {
+	return c.CoreV1().ResourceQuotas(namespace).List(metav1.ListOptions{})
+}
+
+func Secrets(c *kubernetes.Clientset, namespace string) (*corev1.SecretList, error) {
+	return c.CoreV1().Secrets(namespace).List(metav1.ListOptions{})
+}
+
+func ServiceAccounts(c *kubernetes.Clientset, namespace string) (*corev1.ServiceAccountList, error) {
+	return c.CoreV1().ServiceAccounts(namespace).List(metav1.ListOptions{})
+}
+
+func Services(c *kubernetes.Clientset, namespace string) (*corev1.ServiceList, error) {
+	return c.CoreV1().Services(namespace).List(metav1.ListOptions{})
+}
